248e8f2a1ad1e6900c2934615d078d9c2b9806ba
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-12-06 18:04:34 -0800

    Update package.json

--- frontend-find-a-wish/src/App.js
+++ frontend-find-a-wish/src/App.js
@@ -4,11 +4,11 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Navbar, Nav } from 'react-bootstrap';
 
 import Wishlist from "./components/wishlist.component";
-import EditTodo from "./components/edit-todo.component";
+import Profile from "./components/profile.component";
 import HomePage from "./components/homepage.component";
 
 class App extends Component {
-
+  /* Constructor for passing down linking and routing data + text */
   constructor(props) {
     super(props);
     
@@ -26,17 +26,18 @@ class App extends Component {
     }
   }
   render() {
+    /* Navigation bar that is present on every page + routing */
     return (
       <Router>
         <Navbar bg='transparent' variant='dark' expand="md" fixed="top">
           <Nav className="ml-auto">
             <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link href='/organizations'>Organizations</Nav.Link>
             <Nav.Link href='/wishlistform'>Post a Wishlist!</Nav.Link>
+            <Nav.Link href='/organizations'>Organizations</Nav.Link>
           </Nav>
         </Navbar>
         <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} />} />
-        <Route path="/organizations" component={EditTodo} />
+        <Route path="/organizations" component={Profile} />
         <Route path="/wishlistform" component={Wishlist} />
       </Router>
     );
